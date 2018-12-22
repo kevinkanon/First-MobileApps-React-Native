dd8dc5a93f8129262245d46c6ade5b478479832a
Kevin KANON <kanonkevin@yahoo.fr>
2018-12-22 19:59:20 +0100

    end prop and states

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -2,8 +2,8 @@
 
 const API_TOKEN = "f3046bc9073b5d2ed8c3f0c22e5bac0c";
 
-export function getFilmsFromApiWithSearchedText (text) {
-  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+export function getFilmsFromApiWithSearchedText (text, page) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
   return fetch(url)
   .then((response) => response.json())
   .catch((error) => console.error(error))
--- Components/Search.js
+++ Components/Search.js
@@ -8,43 +8,62 @@ class Search extends React.Component {
 
     constructor(props) {
         super(props);  
-        this.searchedText = "" // Initialisation de notre donnée searchedText en dehors du state, car il modifie pas l'aafichage de notre component
+        this.searchedText = ""; // Initialisation de notre donnée searchedText en dehors du state, car il modifie pas l'affichage de notre component
+        this.page = 0;          // compteur pour connaitre la page courante
+        this.totalPages = 0;    // Nombre de pages totales pr savoir si on a atteint la fin des retours de l'API TMDB
         this.state = { 
             films: [],
-            isLoading: false // Par défaut à false car il n'y a pas de chargement tant qu'on ne lance pas de recherche 
+            isLoading: false    // Par défaut à false car il n'y a pas de chargement tant qu'on ne lance pas de recherche 
         }     
     }
+
+    // hydratation de la valeur du text saisie en temps réel dans le Input
+    _searchTextInputChanged(text) { this.searchedText = text; }
     
     // appel de la fonction permettant l'appel à l'API  
     _loadFilms() {
-        //console.log(this.state.searchedText) // Un log pour vérifier qu'on a bien le texte du TextInput
-        if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
-            this.setState({ isLoading: true }) // Lancement du chargement
-            getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
-                this.setState({ 
-                    films: data.results,
-                    isLoading: false // Arrêt du chargement 
+        //console.log(this.state.searchedText)  Un log pour vérifier qu'on a bien le texte du TextInput
+        if (this.searchedText.length > 0) {     // Seulement si le texte recherché n'est pas vide
+            this.setState({ isLoading: true })  // Lancement du chargement
+            getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(data => {
+                this.page = data.page
+                this.totalPages = data.total_pages
+                this.setState({
+                    films: [ ...this.state.films, ...data.results ], // <=> films: this.state.films.concat(data.results)
+                    isLoading: false            // Fin du chargement
                 })
             })
         }
     }
-    // hydratation de la valeur du text saisie en temps réel dans le Input
-    _searchTextInputChanged(text) { this.searchedText = text; }
 
+    /* Ici on va remettre à zéro les films de notre state - _loadFilms() dans le callback de setState pr qu'il ne s'exécute pas de façon asynchrone 
+       il s'éxécutera seulement à pares que page et totalPages soient remis à zero 
+       - setState est une fonction asynchrone   */
+    _searchFilms() {
+        this.page = 0
+        this.totalPages = 0
+        this.setState({
+            films: [],
+            }, () => { 
+                //console.log("Page : " + this.page + " / TotalPages : " + this.totalPages + " / Nombre de films : " + this.state.films.length)
+                this._loadFilms() 
+            }
+        )
+    }
+
+    // Affiche l'icone de chargement
     _displayLoading() {
         if (this.state.isLoading) {
           return (
             <View style={styles.loading_container}>
-              <ActivityIndicator size='large' />
-              {/* Le component ActivityIndicator affiche un icon loader de chargement pr l'expereince utilisateur */}
+              <ActivityIndicator size='large' />{/* Le component ActivityIndicator affiche un icon loader de chargement pr l'expereince utilisateur */}
             </View>
           )
         }
       }
 
     render() {
-        //console.log("RENDER")
-        console.log(this.state.isLoading)
+        //console.log(this.state.isLoading)
         return (
             // Ici on rend à l'écran les éléments graphiques de notre component custom Search
             <View style={styles.main_container}>
@@ -52,14 +71,20 @@ class Search extends React.Component {
                     style={[styles.textinput, { marginBottom:10 }]} 
                     placeholder='titre du film'
                     onChangeText={(text) => this._searchTextInputChanged(text)}
-                    onSubmitEditing={() => this._loadFilms()}   //Validation de la recherche avec le clavier mobile === fonction définie sur le button onPress == onSubmitEditing
+                    onSubmitEditing={() => this._searchFilms()}   //Validation de la recherche avec le clavier mobile === fonction définie sur le button onPress == onSubmitEditing
                 />
-                <Button style={{ height: 50 }} title='Rechercher' onPress={() => this._loadFilms()}/>
+                <Button style={{ height: 50 }} title='Rechercher' onPress={ () => this._searchFilms() }/>
                 
                 <FlatList   //Flatlist permet de crée une liste d'objets
                     data={this.state.films}
-                    keyExtractor={(item) => item.id.toString()}
-                    renderItem={({item}) => <FilmItem film={item}/>}
+                    keyExtractor={ (item) => item.id.toString() }
+                    renderItem={ ({item}) => <FilmItem film={item}/> }
+                    onEndReachedThreshold={0.5}
+                    onEndReached={() => {
+                        if (this.state.films.length > 0 && this.page < this.totalPages) {
+                            this._loadFilms()
+                        }
+                    }}
                  />
                  
                  {this._displayLoading()}
