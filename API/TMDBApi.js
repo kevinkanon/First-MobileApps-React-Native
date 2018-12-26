d56cf87a0870582b76791273c7eb017664afcf02
Kevin KANON <kanonkevin@yahoo.fr>
2018-12-26 03:24:45 +0100

    moment & numeral library instal for detailFilm view with ScrollView

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -2,11 +2,19 @@
 
 const API_TOKEN = "f3046bc9073b5d2ed8c3f0c22e5bac0c";
 
+// Récupération de la liste de tous les film  matchant avec la valeur rechechée par l'utilisateur dans Search component
 export function getFilmsFromApiWithSearchedText (text, page) {
   const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
   return fetch(url)
   .then((response) => response.json())
   .catch((error) => console.error(error))
+}
+
+// Récupération du détail d'un film
+export function getFilmDetailFromApi (id) {
+  return fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + API_TOKEN + '&language=fr')
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
 }
 
 // récupère l'image de associé au film depuis l'API
--- Components/Search.js
+++ Components/Search.js
@@ -54,13 +54,13 @@ class Search extends React.Component {
     // Affiche l'icone de chargement
     _displayLoading() {
         if (this.state.isLoading) {
-          return (
-            <View style={styles.loading_container}>
-              <ActivityIndicator size='large' />{/* Le component ActivityIndicator affiche un icon loader de chargement pr l'expereince utilisateur */}
-            </View>
-          )
+            return (
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size='large' />{/* Le component ActivityIndicator affiche un icon loader de chargement pr l'expereince utilisateur */}
+                </View>
+            )
         }
-      }
+    }
 
     // Affiche les détails d'un film grâce à son ID au click dans la vue FilmDetails
     _displayDetailForFilm = (idFilm) => { 
@@ -88,9 +88,7 @@ class Search extends React.Component {
                     renderItem={ ({item}) => <FilmItem film={item} displayDetailForFilm={this._displayDetailForFilm} /> }
                     onEndReachedThreshold={0.5}
                     onEndReached={() => {
-                        if (this.state.films.length > 0 && this.page < this.totalPages) {
-                            this._loadFilms()
-                        }
+                        if (this.state.films.length > 0 && this.page < this.totalPages) { this._loadFilms() }
                     }
                 }
                 />
@@ -102,26 +100,9 @@ class Search extends React.Component {
 }
 
 const styles = StyleSheet.create ({
-    main_container: {
-        flex: 1
-    },
-    textinput: {
-      marginLeft: 5,
-      marginRight: 5,
-      height: 50,
-      borderColor: '#000000',
-      borderWidth: 1,
-      paddingLeft: 5
-    },
-    loading_container: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        top: 100,
-        bottom: 0,
-        alignItems: 'center',
-        justifyContent: 'center'
-      }
+    main_container: { flex: 1 },
+    textinput: { marginLeft: 5, marginRight: 5, height: 50, borderColor: '#000000', borderWidth: 1, paddingLeft: 5 },
+    loading_container: { position: 'absolute', left: 0, right: 0, top: 100, bottom: 0, alignItems: 'center', justifyContent: 'center' }
   })
 
 export default Search;
